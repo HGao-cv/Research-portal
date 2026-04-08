8a2451f84d19d5a69532955d30d99eacf027215e
Hao Gao <131981929+HGao-cv@users.noreply.github.com>
2026-04-08 22:03:11 +0800

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,17 @@
+// 加载CSS
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://hgao-cv.github.io/research-portal/nav.css';
+document.head.appendChild(link);
+
+// 插入导航栏
+const navHTML = `
+<div class="global-nav">
+  <a href="https://hgao-cv.github.io/research-portal/">🏠 Home</a>
+  <a href="https://hgao-cv.github.io/RAD/">RAD</a>
+  <a href="https://hgao-cv.github.io/RAD2/">RAD2</a>
+</div>
+`;
+
+// 把导航加到页面最顶部
+document.body.insertAdjacentHTML('afterbegin', navHTML);
